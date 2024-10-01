d1154b367d43d7e3771f24576eed86dbdf678bf8
Nitiya <80206062+AsmaanDiPari@users.noreply.github.com>
2024-10-01 16:12:09 +0530

    Update settings.py

--- Django-App/Django-App/settings.py
+++ Django-App/Django-App/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'Django-App.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
